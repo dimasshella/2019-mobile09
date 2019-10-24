2348516139529786c4763260e97a32b3afbf144d
shella <dimasshella04@gmail.com>
2019-10-24 15:52:01 +0700

    tugas 1 done

---
+++ Tugas1/app/src/main/java/com/example/tugas1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tugas1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.tugas1.adapters.WayangJawaAdapter;
+import com.example.tugas1.models.WayangJawa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    //instansiasi Recyclerview
+    RecyclerView rvWayangJawa;
+    //instansiasi list superhero
+    List<WayangJawa> listWayangJawa = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //menyambungkan rvWayangJawa ke layout
+        rvWayangJawa = findViewById(R.id.rvWayangJawa);
+        //Membuat object jawa
+        WayangJawa jawa = new WayangJawa("Oddbods Purpel", R.drawable.ungu);
+        //menambahkan jawa ke listSuperHero
+        listWayangJawa.add(jawa);
+        //membuat object jawa baru
+        jawa = new WayangJawa("Oddbods Green", R.drawable.ijo);
+        //menambahkan jawa baru ke listSuperhero
+        listWayangJawa.add(jawa);
+        //Instansiasi Adapter
+        WayangJawaAdapter wayangJawaAdapter = new WayangJawaAdapter(listWayangJawa);
+        //set adapter dan layoutmanager
+        rvWayangJawa.setAdapter(wayangJawaAdapter);
+        rvWayangJawa.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
---
+++ Tugas1/app/src/main/java/com/example/tugas1/adapters/WayangJawaAdapter.java
@@ -0,0 +1,64 @@
+package com.example.tugas1.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.tugas1.R;
+import com.example.tugas1.models.WayangJawa;
+
+import java.util.List;
+
+public class WayangJawaAdapter extends RecyclerView.Adapter<WayangJawaAdapter.MyViewHolder> {
+    List<WayangJawa> jawaList;
+
+    public WayangJawaAdapter(List<WayangJawa> jawaList) {
+        this.jawaList = jawaList;
+    }
+
+    @NonNull
+    @Override
+    public WayangJawaAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+        View superHeroView = layoutInflater.inflate(R.layout.item_wayang_jawa,parent,false);
+        MyViewHolder viewHolder = new MyViewHolder(superHeroView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WayangJawaAdapter.MyViewHolder holder, int position) {
+        //ambil satu item hero
+        WayangJawa jawa = jawaList.get(position);
+        //set text wayangJawa berdasarkan data dari model hero
+        holder.wayangJawa.setText(jawa.getWayangJawa());
+        holder.gambar.setImageResource(jawa.getGambar());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (jawaList != null) ? jawaList.size() : 0;
+        /*for non shorthand people*/
+        /*if(heroList!=null){
+          return heroList.size();
+        }else{
+          return 0;
+        }*/
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView wayangJawa;
+        public ImageView gambar;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            wayangJawa = itemView.findViewById(R.id.wayangJawa);
+            gambar = itemView.findViewById(R.id.gambar);
+        }
+    }
+}
