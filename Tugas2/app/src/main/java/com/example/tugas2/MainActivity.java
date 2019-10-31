2a3b942ecac8b5b4c61d44c7a1c017626826f599
shella <dimasshella04@gmail.com>
2019-10-31 07:09:29 +0700

    Tugas 2

---
+++ Tugas2/app/src/main/java/com/example/tugas2/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.tugas2.adapters.WayangJawaAdapter;
+import com.example.tugas2.models.WayangJawa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    //instansiasi Recyclerview
+    RecyclerView rvWayangJawa;
+    //instansiasi list superhero
+    List<WayangJawa> listWayangJawa = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvWayangJawa = findViewById(R.id.rvWayangJawa);
+
+        WayangJawa jawa = new WayangJawa("Oddbods Purpel", R.drawable.ungu);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Green", R.drawable.ijo);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Purpel", R.drawable.ungu);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Green", R.drawable.ijo);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Purpel", R.drawable.ungu);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Green", R.drawable.ijo);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Purpel", R.drawable.ungu);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Green", R.drawable.ijo);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Purpel", R.drawable.ungu);
+
+        listWayangJawa.add(jawa);
+
+        jawa = new WayangJawa("Oddbods Green", R.drawable.ijo);
+
+        listWayangJawa.add(jawa);
+
+        WayangJawaAdapter wayangJawaAdapter = new WayangJawaAdapter(listWayangJawa);
+
+        rvWayangJawa.setAdapter(wayangJawaAdapter);
+        rvWayangJawa.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+    }
+}
