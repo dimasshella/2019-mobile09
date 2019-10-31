ba73965663391511b8d039137fc753482be6cc07
shella <dimasshella04@gmail.com>
2019-10-31 07:27:11 +0700

    Tugas3

---
+++ Tugas3/app/src/main/java/com/example/tugas3/models/WayangJawa.java
@@ -0,0 +1,27 @@
+package com.example.tugas3.models;
+
+public class WayangJawa {
+    public String wayangJawa;
+    public int gambar;
+
+    public WayangJawa(String wayangJawa, int gambar) {
+        this.wayangJawa = wayangJawa;
+        this.gambar = gambar;
+    }
+
+    public String getWayangJawa() {
+        return wayangJawa;
+    }
+
+    public void setWayangJawa(String wayangJawa) {
+        this.wayangJawa = wayangJawa;
+    }
+
+    public int getGambar() {
+        return gambar;
+    }
+
+    public void setGambar(int gambar) {
+        this.gambar = gambar;
+    }
+}
